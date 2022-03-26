961f146e61e27c899950065d0128e6eb1076d26a
sulagna <sulagna@dhcp-117.cs.mtholyoke.edu>
2022-03-25 22:21:16 -0400

    dora's commit

---
+++ Sride/ProfileViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileViewController.swift
+//  Sride
+//
+//  Created by sulagna on 3/25/22.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class ProfileViewController: UIViewController {
+
+
+    
+    @IBOutlet weak var profileTableView: UITableView!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+}
