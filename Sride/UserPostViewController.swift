5cbdec5911cddf1b7579bc308b0fe77621d76bb0
Autumn Nguyen <autumn-yng@users.noreply.github.com>
2022-04-01 18:17:36 -0400

    create UserPostStoryBoard

---
+++ Sride/UserPostViewController.swift
@@ -0,0 +1,44 @@
+//
+//  UserPostViewController.swift
+//  Sride
+//
+//  Created by Autumn Y Ngoc on 4/1/22.
+//
+
+import UIKit
+import Parse
+
+class UserPostViewController: UIViewController {
+    
+//    @IBOutlet weak var tableView: UITableView!
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        tableView.delegate = self
+//        tableView.dataSource = self
+
+//  //       Do any additional setup after loading the view.
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
